5a3883a344d52588273e72fbfddea9947f22ac56
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-12-28 09:18:25 +0100

    Rename Readme.md to readme.rst because we github can't handle rst in .md files correctly

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('CHANGELOG.md') as changelog_file:
