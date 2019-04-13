5586e94b1dff17d4fc96d4ca5ba9e6a9c644d799
Usman Nasir <usman@cyberpersons.com>
2019-04-13 23:01:48 +0500

    server_type does not accept strings

--- examples/create_server.py
+++ examples/create_server.py
@@ -2,7 +2,7 @@ from hcloud import Client
 from hcloud.images.domain import Image
 
 client = Client(token="{YOUR_API_TOKEN}")  # Please paste your API token here between the quotes
-response = client.servers.create(name="my-server", server_type="cx11", image=Image(name="ubuntu-18.04"))
+response = client.servers.create(name="my-server", server_type=ServerType("cx11"), image=Image(name="ubuntu-18.04"))
 server = response.server
 print(server)
 print("Root Passwort" + response.root_password)
