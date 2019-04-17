f52e32bcd46bf399b2aba3420855226445115cf9
Usman Nasir <usman@cyberpersons.com>
2019-04-17 16:17:54 +0500

    Fix create_server example (#39)

--- examples/create_server.py
+++ examples/create_server.py
@@ -1,8 +1,9 @@
 from hcloud import Client
 from hcloud.images.domain import Image
+from hcloud.server_types.domain import ServerType
 
 client = Client(token="{YOUR_API_TOKEN}")  # Please paste your API token here between the quotes
-response = client.servers.create(name="my-server", server_type="cx11", image=Image(name="ubuntu-18.04"))
+response = client.servers.create(name="my-server", server_type=ServerType("cx11"), image=Image(name="ubuntu-18.04"))
 server = response.server
 print(server)
 print("Root Passwort" + response.root_password)
