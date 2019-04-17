2a90551fb1c4d9d8a6aea5d8b6601a7c1360494d
Usman Nasir <usman@cyberpersons.com>
2019-04-17 15:11:41 +0500

    missing ServerType import

--- examples/create_server.py
+++ examples/create_server.py
@@ -1,5 +1,6 @@
 from hcloud import Client
 from hcloud.images.domain import Image
+from hcloud.server_types.domain import ServerType
 
 client = Client(token="{YOUR_API_TOKEN}")  # Please paste your API token here between the quotes
 response = client.servers.create(name="my-server", server_type=ServerType("cx11"), image=Image(name="ubuntu-18.04"))
