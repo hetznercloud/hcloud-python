cf64e549a2b28aea91062dea67db8733b4ecdd6f
Jonas L <jooola@users.noreply.github.com>
2023-07-11 14:23:30 +0200

    feat: add details to raise exceptions (#240)

--- tests/unit/actions/test_domain.py
+++ tests/unit/actions/test_domain.py
@@ -1,7 +1,14 @@
 import datetime
 from datetime import timezone
 
-from hcloud.actions.domain import Action
+import pytest
+
+from hcloud.actions.domain import (
+    Action,
+    ActionException,
+    ActionFailedException,
+    ActionTimeoutException,
+)
 
 
 class TestAction:
@@ -14,4 +21,44 @@ class TestAction:
         )
         assert action.finished == datetime.datetime(
             2016, 3, 30, 23, 50, tzinfo=timezone.utc
+        )
+
+
+def test_action_exceptions():
+    with pytest.raises(
+        ActionException,
+        match=r"The pending action failed: Server does not exist anymore",
+    ):
+        raise ActionFailedException(
+            action=Action(
+                **{
+                    "id": 1084730887,
+                    "command": "change_server_type",
+                    "status": "error",
+                    "progress": 100,
+                    "resources": [{"id": 34574042, "type": "server"}],
+                    "error": {
+                        "code": "server_does_not_exist_anymore",
+                        "message": "Server does not exist anymore",
+                    },
+                    "started": "2023-07-06T14:52:42+00:00",
+                    "finished": "2023-07-06T14:53:08+00:00",
+                }
+            )
+        )
+
+    with pytest.raises(ActionException, match=r"The pending action timed out"):
+        raise ActionTimeoutException(
+            action=Action(
+                **{
+                    "id": 1084659545,
+                    "command": "create_server",
+                    "status": "running",
+                    "progress": 50,
+                    "started": "2023-07-06T13:58:38+00:00",
+                    "finished": None,
+                    "resources": [{"id": 34572291, "type": "server"}],
+                    "error": None,
+                }
+            )
         )
