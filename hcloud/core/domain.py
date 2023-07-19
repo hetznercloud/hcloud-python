cec9b6790815c935fecb2a7bf292c50ad37b3739
jo <ljonas@riseup.net>
2023-07-19 09:40:49 +0200

    feat: add __repr__ method to domains

--- hcloud/core/domain.py
+++ hcloud/core/domain.py
@@ -9,6 +9,10 @@ class BaseDomain:
         supported_data = {k: v for k, v in data.items() if k in cls.__slots__}
         return cls(**supported_data)
 
+    def __repr__(self) -> str:
+        kwargs = [f"{key}={getattr(self, key)!r}" for key in self.__slots__]
+        return f"{self.__class__.__qualname__}({', '.join(kwargs)})"
+
 
 class DomainIdentityMixin:
     __slots__ = ()
