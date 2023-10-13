036b52fe51bcbb6b610c0c99ca224d3c4bbfc68d
Jonas L <jooola@users.noreply.github.com>
2023-10-13 14:22:57 +0200

    feat: add deprecation field to Iso (#318)
    See these changelog entries for the announcement:
    
    -
    https://docs.hetzner.cloud/changelog#2023-10-12-deprecation-info-for-isos
    -
    https://docs.hetzner.cloud/changelog#2023-10-12-field-deprecated-on-isos-is-now-deprecated

--- hcloud/isos/domain.py
+++ hcloud/isos/domain.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dateutil.parser import isoparse
 
 from ..core import BaseDomain, DomainIdentityMixin
+from ..deprecation import DeprecationInfo
 
 
 class Iso(BaseDomain, DomainIdentityMixin):
@@ -19,10 +20,21 @@ class Iso(BaseDomain, DomainIdentityMixin):
     :param architecture: str, None
            CPU Architecture that the ISO is compatible with. None means that the compatibility is unknown. Choices: `x86`, `arm`
     :param deprecated: datetime, None
-           ISO 8601 timestamp of deprecation, None if ISO is still available. After the deprecation time it will no longer be possible to attach the ISO to servers.
+           ISO 8601 timestamp of deprecation, None if ISO is still available. After the deprecation time it will no longer be possible to attach the ISO to servers. This field is deprecated. Use `deprecation` instead.
+    :param deprecation: :class:`DeprecationInfo <hcloud.deprecation.domain.DeprecationInfo>`, None
+        Describes if, when & how the resources was deprecated. If this field is set to None the resource is not
+        deprecated. If it has a value, it is considered deprecated.
     """
 
-    __slots__ = ("id", "name", "type", "architecture", "description", "deprecated")
+    __slots__ = (
+        "id",
+        "name",
+        "type",
+        "architecture",
+        "description",
+        "deprecated",
+        "deprecation",
+    )
 
     def __init__(
         self,
@@ -32,6 +44,7 @@ class Iso(BaseDomain, DomainIdentityMixin):
         architecture: str | None = None,
         description: str | None = None,
         deprecated: str | None = None,
+        deprecation: dict | None = None,
     ):
         self.id = id
         self.name = name
@@ -39,3 +52,6 @@ class Iso(BaseDomain, DomainIdentityMixin):
         self.architecture = architecture
         self.description = description
         self.deprecated = isoparse(deprecated) if deprecated else None
+        self.deprecation = (
+            DeprecationInfo.from_dict(deprecation) if deprecation is not None else None
+        )
--- tests/unit/isos/test_domain.py
+++ tests/unit/isos/test_domain.py
@@ -1,14 +1,38 @@
 from __future__ import annotations
 
-import datetime
-from datetime import timezone
+from datetime import datetime, timezone
+
+import pytest
 
 from hcloud.isos import Iso
 
 
 class TestIso:
-    def test_deprecated_is_datetime(self):
-        iso = Iso(id=1, deprecated="2016-01-30T23:50+00:00")
-        assert iso.deprecated == datetime.datetime(
-            2016, 1, 30, 23, 50, tzinfo=timezone.utc
+    @pytest.fixture()
+    def deprecated_iso(self):
+        return Iso(
+            **{
+                "id": 10433,
+                "name": "vyos-1.4-rolling-202111150317-amd64.iso",
+                "description": "VyOS 1.4 (amd64)",
+                "type": "public",
+                "deprecation": {
+                    "announced": "2023-10-05T08:27:01Z",
+                    "unavailable_after": "2023-11-05T08:27:01Z",
+                },
+                "architecture": "x86",
+                "deprecated": "2023-11-05T08:27:01Z",
+            }
+        )
+
+    def test_deprecation(self, deprecated_iso: Iso):
+        assert deprecated_iso.deprecated == datetime(
+            2023, 11, 5, 8, 27, 1, tzinfo=timezone.utc
+        )
+        assert deprecated_iso.deprecation is not None
+        assert deprecated_iso.deprecation.announced == datetime(
+            2023, 10, 5, 8, 27, 1, tzinfo=timezone.utc
+        )
+        assert deprecated_iso.deprecation.unavailable_after == datetime(
+            2023, 11, 5, 8, 27, 1, tzinfo=timezone.utc
         )
