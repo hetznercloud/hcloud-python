39bf073fa0739e71486d46677602e7da5835b0e8
Jürgen Kreileder <jk@blackdown.de>
2023-12-23 00:34:40 +0100

    Fix failing pre-commit check
    Signed-off-by: Jürgen Kreileder <jk@blackdown.de>

--- hcloud/__init__.py
+++ hcloud/__init__.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
 
-from ._client import Client as Client # pylint: disable=C0414
-from ._exceptions import APIException as APIException, HCloudException as HCloudException # pylint: disable=C0414
+from ._client import Client as Client  # noqa pylint: disable=C0414
+from ._exceptions import (  # noqa pylint: disable=C0414
+    APIException as APIException,
+    HCloudException as HCloudException,
+)
