6c247eebebbab8d451056a8804ed73bb60b51517
Jürgen Kreileder <jk@blackdown.de>
2023-12-22 20:46:23 +0100

    Export Client, APIException and HCloudException
    Fixes https://github.com/hetznercloud/hcloud-python/issues/345
    
    Signed-off-by: Jürgen Kreileder <jk@blackdown.de>

--- hcloud/__init__.py
+++ hcloud/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-from ._client import Client  # noqa
-from ._exceptions import APIException, HCloudException  # noqa
+from ._client import Client as Client # pylint: disable=C0414
+from ._exceptions import APIException as APIException, HCloudException as HCloudException # pylint: disable=C0414
