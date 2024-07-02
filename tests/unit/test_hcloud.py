db37e633ebbf73354d3b2f4858cf3eebf173bfbc
Jonas L <jooola@users.noreply.github.com>
2024-07-02 12:11:01 +0200

    feat!: remove deprecated `hcloud.hcloud` module (#401)
    The module was previously deprecated.

--- tests/unit/test_hcloud.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import pytest
-
-
-def test_deprecated_hcloud_hcloud_module():
-    with pytest.deprecated_call():
-        from hcloud.hcloud import Client  # noqa
