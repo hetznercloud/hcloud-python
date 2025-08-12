25f197896f7cb8a9dc8045ac2b1941ed55c93ea3
jo <ljonas@riseup.net>
2025-08-12 11:58:43 +0200

    feat: renamed ClientEntityBase to BaseResourceClient

--- hcloud/core/__init__.py
+++ hcloud/core/__init__.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
-from .client import BoundModelBase, ClientEntityBase
+from .client import BaseResourceClient, BoundModelBase, ClientEntityBase
 from .domain import BaseDomain, DomainIdentityMixin, Meta, Pagination
 
 __all__ = [
+    "BaseDomain",
+    "BaseResourceClient",
     "BoundModelBase",
     "ClientEntityBase",
-    "BaseDomain",
     "DomainIdentityMixin",
     "Meta",
     "Pagination",
