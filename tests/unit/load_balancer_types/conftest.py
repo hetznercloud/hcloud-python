1c8cfed9bc6e05fc26ba45e0a73e0e9dfed3c0e7
Jonas Lammler <jonas.lammler@hetzner-cloud.de>
2026-07-20 18:28:09 +0200

    test: refresh load balancer types tests

--- tests/unit/load_balancer_types/conftest.py
+++ tests/unit/load_balancer_types/conftest.py
@@ -4,114 +4,49 @@ import pytest
 
 
 @pytest.fixture()
-def load_balancer_type_response():
+def load_balancer_type1():
     return {
-        "load_balancer_type": {
-            "id": 1,
-            "name": "LB11",
-            "description": "LB11",
-            "max_connections": 1,
-            "max_services": 1,
-            "max_targets": 1,
-            "max_assigned_certificates": 1,
-            "deprecated": None,
-            "prices": [
-                {
-                    "location": "fsn1",
-                    "price_hourly": {
-                        "net": "1.0000000000",
-                        "gross": "1.1900000000000000",
-                    },
-                    "price_monthly": {
-                        "net": "1.0000000000",
-                        "gross": "1.1900000000000000",
-                    },
-                }
-            ],
-        }
+        "id": 1,
+        "name": "lb11",
+        "description": "LB11",
+        "max_connections": 10000,
+        "max_services": 5,
+        "max_targets": 25,
+        "max_assigned_certificates": 10,
+        "deprecation": {
+            "announced": "2023-06-01T00:00:00Z",
+            "unavailable_after": "2023-09-01T00:00:00Z",
+        },
+        "prices": [
+            {
+                "location": "fsn1",
+                "price_hourly": {"net": "0.0120", "gross": "0.0120"},
+                "price_monthly": {"net": "7.4900", "gross": "7.4900"},
+                "price_per_tb_traffic": {"net": "1.0000", "gross": "1.0000"},
+                "included_traffic": 21990232555520,
+            },
+        ],
     }
 
 
 @pytest.fixture()
-def two_load_balancer_types_response():
+def load_balancer_type2():
     return {
-        "load_balancer_types": [
+        "id": 2,
+        "name": "lb21",
+        "description": "LB21",
+        "max_connections": 20000,
+        "max_services": 15,
+        "max_targets": 75,
+        "max_assigned_certificates": 25,
+        "deprecation": None,
+        "prices": [
             {
-                "id": 1,
-                "name": "LB11",
-                "description": "LB11D",
-                "max_connections": 1,
-                "max_services": 1,
-                "max_targets": 1,
-                "max_assigned_certificates": 1,
-                "deprecated": None,
-                "prices": [
-                    {
-                        "location": "fsn1",
-                        "price_hourly": {
-                            "net": "1.0000000000",
-                            "gross": "1.1900000000000000",
-                        },
-                        "price_monthly": {
-                            "net": "1.0000000000",
-                            "gross": "1.1900000000000000",
-                        },
-                    }
-                ],
+                "location": "fsn1",
+                "price_hourly": {"net": "0.0344", "gross": "0.0344"},
+                "price_monthly": {"net": "21.4900", "gross": "21.4900"},
+                "price_per_tb_traffic": {"net": "1.0000", "gross": "1.0000"},
+                "included_traffic": 21990232555520,
             },
-            {
-                "id": 2,
-                "name": "LB21",
-                "description": "LB21D",
-                "max_connections": 2,
-                "max_services": 2,
-                "max_targets": 2,
-                "max_assigned_certificates": 2,
-                "deprecated": None,
-                "prices": [
-                    {
-                        "location": "fsn1",
-                        "price_hourly": {
-                            "net": "1.0000000000",
-                            "gross": "1.1900000000000000",
-                        },
-                        "price_monthly": {
-                            "net": "1.0000000000",
-                            "gross": "1.1900000000000000",
-                        },
-                    }
-                ],
-            },
-        ]
-    }
-
-
-@pytest.fixture()
-def one_load_balancer_types_response():
-    return {
-        "load_balancer_types": [
-            {
-                "id": 2,
-                "name": "LB21",
-                "description": "LB21D",
-                "max_connections": 2,
-                "max_services": 2,
-                "max_targets": 2,
-                "max_assigned_certificates": 2,
-                "deprecated": None,
-                "prices": [
-                    {
-                        "location": "fsn1",
-                        "price_hourly": {
-                            "net": "1.0000000000",
-                            "gross": "1.1900000000000000",
-                        },
-                        "price_monthly": {
-                            "net": "1.0000000000",
-                            "gross": "1.1900000000000000",
-                        },
-                    }
-                ],
-            }
-        ]
+        ],
     }
